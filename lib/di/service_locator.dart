a7589a56e8786fb9642a7b43ae8cc326ada32bfc
Ansar <muhammedansar7787@gmail.com>
2026-01-04 21:04:23 +0530

    feat: register admin dependencies in service locator

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -3,6 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:smart_study_plan/features/admin_panel/data/datasources/admin_local_datasource.dart';
+import 'package:smart_study_plan/features/admin_panel/data/datasources/admin_remote_datasource.dart';
+import 'package:smart_study_plan/features/admin_panel/data/repositories/admin_repository_impl.dart';
+import 'package:smart_study_plan/features/admin_panel/domain/repositories/admin_repository.dart';
+import 'package:smart_study_plan/features/admin_panel/domain/usecases/delete_user_admin.dart';
+import 'package:smart_study_plan/features/admin_panel/domain/usecases/get_admin_stats.dart';
+import 'package:smart_study_plan/features/admin_panel/domain/usecases/get_all_users.dart';
+import 'package:smart_study_plan/features/admin_panel/presentation/bloc/admin_bloc.dart';
 import 'package:smart_study_plan/features/user_management/data/datasources/user_local_datasource.dart';
 import 'package:smart_study_plan/features/user_management/data/datasources/user_remote_datasource.dart';
 import 'package:smart_study_plan/features/user_management/data/repositories/user_repository_impl.dart';
@@ -75,6 +83,44 @@ Future<void> setupServiceLocator() async {
       loginUserUseCase: getIt<LoginUserUseCase>(),
       getCurrentUserUseCase: getIt<GetCurrentUserUseCase>(),
       logoutUserUseCase: getIt<LogoutUserUseCase>(),
+    ),
+  );
+
+  // Admin Local Datasource
+  final adminLocalDatasource = AdminLocalDatasourceImpl();
+  await adminLocalDatasource.init();
+  getIt.registerSingleton<AdminLocalDatasource>(adminLocalDatasource);
+
+  // Admin Remote Datasource
+  getIt.registerSingleton<AdminRemoteDatasource>(
+    AdminRemoteDatasourceImpl(FirebaseFirestore.instance),
+  );
+
+  // Admin Repository
+  getIt.registerSingleton<AdminRepository>(
+    AdminRepositoryImpl(
+      remoteDatasource: getIt<AdminRemoteDatasource>(),
+      localDatasource: getIt<AdminLocalDatasource>(),
+    ),
+  );
+
+  // Admin UseCases
+  getIt.registerSingleton<GetAllUsersUseCase>(
+    GetAllUsersUseCase(getIt<AdminRepository>()),
+  );
+  getIt.registerSingleton<GetAdminStatsUseCase>(
+    GetAdminStatsUseCase(getIt<AdminRepository>()),
+  );
+  getIt.registerSingleton<DeleteUserAdminUseCase>(
+    DeleteUserAdminUseCase(getIt<AdminRepository>()),
+  );
+
+  // Admin Bloc
+  getIt.registerSingleton<AdminBloc>(
+    AdminBloc(
+      getAllUsersUseCase: getIt<GetAllUsersUseCase>(),
+      getAdminStatsUseCase: getIt<GetAdminStatsUseCase>(),
+      deleteUserAdminUseCase: getIt<DeleteUserAdminUseCase>(),
     ),
   );
 
